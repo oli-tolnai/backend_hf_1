8546c4453298d51453c5650d883a5242da85e4d1
oli-tolnai <oli.tolnai3@gmail.com>
2024-11-20 13:38:55 +0100

    DanubeLevelLogic updated and done

--- backend_hf_1.Logic/DanubeLevelLogic.cs
+++ backend_hf_1.Logic/DanubeLevelLogic.cs
@@ -16,7 +16,18 @@ namespace backend_hf_1.Logic
         public void AddDanubeLevel(DanubeLevelCreateDto dto)
         {
             DanubeLevel d = new DanubeLevel(dto.Date, dto.Value);
-            repo.Create(d);
+
+            if (repo.GetAll().FirstOrDefault(x => x.Date.Date == d.Date.Date) == null) 
+            {
+                repo.Create(d);
+            }
+            else
+            {
+                //TODO: Implement exception handling
+                //throw new System.Exception("There is already a record for this date");
+            }
+
+
         }
 
 
