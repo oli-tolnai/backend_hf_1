4b6d720c61893fb93d572fa7694c51e63b54b0fe
oli-tolnai <oli.tolnai3@gmail.com>
2024-11-20 17:59:31 +0100

    PathInput and Convert Json into DanubeLevelCreateDto List

--- backend_hf_1.ClientConsole/Program.cs
+++ backend_hf_1.ClientConsole/Program.cs
@@ -1,12 +1,34 @@
 ﻿using backend_hf_1.Endpoint.Controllers;
+using backend_hf_1.Entities.Dtos;
+using Newtonsoft.Json;
 
 namespace backend_hf_1.ClientConsole
 {
-    internal class Program
+    public class Program
     {
+
+
         static void Main(string[] args)
         {
+            var data = JsonIntoDb(PathInput());
+        }
+
+        public static List<DanubeLevelCreateDto> JsonIntoDb(string path)
+        {
+            string jsonString = File.ReadAllText(path);
+            Console.WriteLine(jsonString);
+
+            //deserialize jsonString into a list of DanubeLevelCreateDto
             
+            List<DanubeLevelCreateDto> danubeLevelData = JsonConvert.DeserializeObject<List<DanubeLevelCreateDto>>(jsonString);
+
+            return danubeLevelData;
+        }
+        static string PathInput()
+        {
+            System.Console.Write("Please enter the path of the JSON file: ");
+            return System.Console.ReadLine();
         }
     }
+
 }
