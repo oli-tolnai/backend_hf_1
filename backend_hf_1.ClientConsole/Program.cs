b4035a5827e3a6945a7ed1cfc6b5eed0da2c140f
oli-tolnai <oli.tolnai3@gmail.com>
2024-11-20 17:38:23 +0100

    ClientConsole created

---
+++ backend_hf_1.ClientConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using backend_hf_1.Endpoint.Controllers;
+
+namespace backend_hf_1.ClientConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
