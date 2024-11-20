726dbb066a34a49aa85be91dee4ccb6e31f74755
oli-tolnai <oli.tolnai3@gmail.com>
2024-11-20 10:21:26 +0100

    DanbueLevel correction

--- backend_hf_1.Entities/Entity Models/DanubeLevel.cs
+++ backend_hf_1.Entities/Entity Models/DanubeLevel.cs
@@ -1,25 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Security.Principal;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace backend_hf_1.Entities.Entity_Models
 {
     public class DanubeLevel
     {
-        public DanubeLevel(DateTime date, int level)
+        public DanubeLevel(DateTime date, int value)
         {
             Id = Guid.NewGuid().ToString();
             Date = date;
-            Level = level;
+            Value = value;
         }
 
-
+        [StringLength(50)]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public string Id { get; set; }
+
+
+        [JsonPropertyName("date")]
         public DateTime Date { get; set; }
-        public int Level { get; set; }
+
+        [JsonPropertyName("value")]
+        public int Value { get; set; }
   
     }
 }
